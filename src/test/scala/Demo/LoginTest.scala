48c5c6b53f692fcd16954c65cc59b3b1e46950ee
jairsilva0742 <52806396+jairsilva0742@users.noreply.github.com>
2025-06-24 22:04:42 -0500

    Update2

--- src/test/scala/Demo/LoginTest.scala
+++ src/test/scala/Demo/LoginTest.scala
@@ -21,6 +21,13 @@ class LoginTest extends Simulation{
       .check(status.is(200))
       .check(jsonPath("$.token").saveAs("authToken"))
     )
+  .exec(
+      http("Create Contact")
+        .post(s"contacts")
+        .header("Authorization", "Bearer ${authToken}")
+        .body(StringBody(s"""{"firstName": "Javier","lastName": "Silva","birthdate": "1960-12-01","email": "jdoe@fake.com","phone": "8005555555","street1": "1 Main St.","street2": "Apartment A","city": "Anytown","stateProvince": "KS","postalCode": "12345","country": "USA"}""")).asJson
+        .check(status.is(201))
+    )
 
   // 3 Load Scenario
   setUp(
