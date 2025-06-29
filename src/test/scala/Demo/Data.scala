473ee396de9663efa6ee77685d6d11ec5d13d76c
jairsilva0742 <52806396+jairsilva0742@users.noreply.github.com>
2025-06-28 20:09:33 -0500

    Nuevo Correo Data.scala

--- src/test/scala/Demo/Data.scala
+++ src/test/scala/Demo/Data.scala
@@ -2,6 +2,6 @@ package Demo
 
 object Data{
     val url= "https://thinking-tester-contact-list.herokuapp.com/"
-    val email = "jsilvah@unal.edu.co"
+    val email = "jair.silva@udea.edu.co"
     val password = "123456789"
 }
